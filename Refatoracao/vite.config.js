e4b8c58877bda9a81c2472ced271854ecd09312a
Cauã <dev.cauaraphael@gmail.com>
2025-08-13 00:25:33 -0300

    corrigindo o caminho do vite.config

--- Refatoracao/vite.config.js
+++ Refatoracao/vite.config.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "/Site_Portfolio", 
+  base: "/Site_Portfolio/", 
   server: {
     host: "::",
     port: 8080,
